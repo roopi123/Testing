479c61e55498b4734ab8c01b5280e34a92a8c9ef
roopi123 <roopesh.geni@gmail.com>
2022-03-22 00:30:26 +0530

    second from chrome

---
+++ src/test/java/Group/Hancom/AppTest2.java
@@ -0,0 +1,13 @@
+package Group.Hancom;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest2 {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}

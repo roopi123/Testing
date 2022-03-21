8cceecb8bc4c3642a363ae3770e17b0b3844fb8a
roopi123 <roopesh.geni@gmail.com>
2022-03-22 00:54:08 +0530

    from main

--- src/test/java/Group/Hancom/AppTest.java
+++ src/test/java/Group/Hancom/AppTest.java
@@ -4,10 +4,42 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class AppTest {
-    
-  @Test
-  public void shouldAnswerWithTrue() {
-    assertTrue(true);
-  }
+@Test
+public void shouldAnswerWithTrue() {
+  assertTrue(true);
 }
+   @Test
+public void ssshouldAnswerWithTrue() {
+  assertTrue(true);
+}
+@Test
+public void shoulsdAnswerWithTrue() {
+  assertTrue(true);
+}
+@Test
+public void shouldAnswerWithTrue() {
+  assertTrue(true);
+}
+@Test
+public void shouldAnswerWithTrue() {
+  assertTrue(true);
+}
+@Test
+public void shouldAnswerWithTrue() {
+  assertTrue(true);
+}
+@Test
+public void shouldAnswerWithTrue() {
+  assertTrue(true);
+}
+
+@Test
+public void shouldAnswerWithTrue() {
+  assertTrue(true);
+}
+@Test
+public void shouldAnswerWithTrue() {
+  assertTrue(true);
+}
+@Test
+public void shouldAnswerW
